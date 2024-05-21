d2760c04779f781e1774484177d16f2cc6173776
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-05-21 18:59:40 +0530

    change font to oswald

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <>
-    
+    <main className="p-4">
+    <h1 className="text-2xl">Say My Name Manoj Lasantha</h1>
     <Header/>
     
-    </>
+    </main>
   );
 }
