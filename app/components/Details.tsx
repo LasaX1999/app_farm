5aac61fb048d7fcf8821ffe088f9952fd08486cb
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-05-21 22:15:14 +0530

    cart button width change

--- app/components/Details.tsx
+++ app/components/Details.tsx
@@ -7,7 +7,7 @@ function Details() {
   return (
     <div className="max-w-7xl mx-auto mt-20">
       <div className="grid grid-cols-1 lg:grid-cols-2">
-        <div className="shadow-md relative h-96 overflow-hidden aspect-ratio-1">
+        <div className="shadow-md relative rounded-md h-96 overflow-hidden aspect-ratio-1">
           <Image src={image1} className=" object-cover" alt={"pics"} />
         </div>
 
@@ -25,7 +25,7 @@ function Details() {
             </div>
 
             <div className="mt-6">
-                <button className="w-full bg-sky-700 py-3 rounded-md">Add Cart</button>
+                <button className="w-20 bg-sky-400 py-2 rounded-md">Add Cart</button>
 
             </div>
 
