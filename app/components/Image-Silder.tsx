e56d48ad4c85f969a663dc71d820be229352f578
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-05-23 21:29:57 +0530

    hide scroll bar

--- app/components/Image-Silder.tsx
+++ app/components/Image-Silder.tsx
@@ -6,8 +6,10 @@ import { ImagesSlider } from "./ui/images-slider";
 export default function ImagesSliderDemo() {
   const images = [
     "https://images.unsplash.com/photo-1605000797499-95a51c5269ae?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1626984232613-f20f15589bee?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1519897831810-a9a01aceccd1?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1563201515-adbe35c669c5?q=80&w=1474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ",
+    "https://images.unsplash.com/photo-1568623970736-27bea63b4489?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "",
   ];
   return (
     <ImagesSlider className="h-[40rem]" images={images}>
@@ -21,15 +23,15 @@ export default function ImagesSliderDemo() {
           y: 0,
         }}
         transition={{
-          duration: 0.6,
+          duration: 0.3,
         }}
         className="z-50 flex flex-col justify-center items-center"
       >
         <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
-          The hero section slideshow <br /> nobody asked for
+        An online produce marketplace to buy and sell from the comfort of your own home.
         </motion.p>
-        <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-          <span>Join now →</span>
+        <button className="px-8 py-3 backdrop-blur-sm border bg-green-600/20 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+          <span className="text-xl">Shop Now</span>
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
         </button>
       </motion.div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,17 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Card from "./components/Card";
 import ImageSlider from './components/Image-Silder'
+import { FaApple, FaShop } from "react-icons/fa6";
+import { FaProcedures } from "react-icons/fa";
+import { GiFruitTree } from "react-icons/gi";
+import { CiDeliveryTruck } from "react-icons/ci";
 
 export default function Home() {
   return (
     <main>
       <Header />
 
-      <div className="flex flex-col items-center justify-center mt-10 space-y-4">
+      <div className="flex flex-col items-center justify-center mt-2 mb-2 ">
         <h1 className="text-4xl text-orange-600">Shop Local Products</h1>
 
         <p className="text-center text-gray-600 text-xl">
@@ -20,7 +24,37 @@ export default function Home() {
 
     <ImageSlider/>
 
-      <div className="p-8 flex ">
+
+    <div className="mt-8 mb-8 flex flex-col justify-center items-center gap-4 text-3xl">
+      
+      <h1 className="lg:text-8xl text-4xl mb-8 "> Selling with us - 3 easy steps </h1>
+
+      <div className="flex gap-20  p-4 ">
+      <div className="flex flex-col gap-4 justify-center items-center">
+        <FaShop className="text-[100px]"/>
+        <h1 className=" underline-offset-4 underline text-[#F27052]">1. Open Your Shop</h1>
+        <p className="font-thin text-2xl">Add your shop name <br /> a description and some <br />
+         contact details.</p>
+      </div>
+
+      <div className="flex flex-col gap-4 justify-center items-center">
+        <GiFruitTree className="text-[100px]"/>
+        <h1 className=" underline-offset-4 underline text-[#F27052]">2. Add Your Products</h1>
+        <p className=" font-thin text-2xl">Create products, set stock <br /> levels and open an order <br /> cycle
+        </p>
+      </div>
+
+
+      <div className="flex flex-col gap-4 justify-center items-center">
+        <CiDeliveryTruck className="text-[110px]"/>
+        <h1 className=" underline-offset-4 underline text-[#F27052] ">3. Plan Your Deliveries</h1>
+        <p className=" font-thin  text-2xl ">Set up payment methods <br />  and add your collection and <br /> delivery options.</p>
+      </div>
+      </div>
+
+    </div>
+
+      {/* <div className="p-8 flex ">
        
         <div className="mx-auto grid grid-cols-1 lg:grid-cols-3 gap-16">
         
@@ -30,7 +64,7 @@ export default function Home() {
        
           
         </div>
-      </div>
+      </div> */}
     </main>
   );
 }
