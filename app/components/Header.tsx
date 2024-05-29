5c8efe0beadd168d54fa426db60a2dbdc3f0cc09
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-05-29 10:14:10 +0530

    make responsive navbar for sm devices

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,15 +11,16 @@ function Header() {
       <div className="max-w-7xl mx-auto flex justify-between  items-center">
 
         <div className="flex items-center">
-          <h1 className="ml-2 text-2xl lg:text-3xl">
+          <h1 className="ml-2 text-xl lg:text-3xl">
             🥬FARM TO <span className="text-[#86D72F]">MARKET</span>
           </h1>
         </div>
 
         <div className="flex items-center relative ">
-          <Link href={'/shop'} className="text-xl mr-6 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Shop </Link>
-          <Link href={'/admin'} className="text-xl mr-6 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Sell </Link>
-          <Link href={'/about'} className="text-xl mr-6 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">About Us </Link>
+          <Link href={'/shop'} className="lg:text-xl text-[18px] mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Shop </Link>
+          <Link href={'/admin'} className="lg:text-xl text-[18px]  mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Sell </Link>
+          <Link href={'/about'} className="lg:text-xl text-[18px]  mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">About  </Link>
+          <Link href={'/contact'} className="lg:text-xl text-[18px]  mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Contact </Link>
          <Link href={'/cart'}> <HiShoppingCart className="text-3xl cursor-pointer hover:scale-110" /></Link>
           <div className="ml-2 bg-green-500 rounded-full w-5 h-5 flex items-center justify-center text-white text-sm font-semibold">1</div>
           <div className="ml-4">
---
+++ app/contact/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { ValidationError, useForm } from "@formspree/react";
+import Link from "next/link";
+import React, { useState } from "react";
+import Header from "../components/Header";
+
+export default function Form() {
+  const [state, handleSubmit] = useForm("xbjnwwzj");
+  if (state.succeeded) {
+    return <div className="bg-blue-200 p-8 ">
+      <div className="flex-col justify-center">
+        <h1 className="text-2xl text-black ">Thanks!</h1>
+        <p className="text-lg">The form was submitted successfully.</p>
+        <Link href={'/Contact'} className="font-medium text-xl text-[#E5122E]">Go Back</Link>
+      </div>
+    </div>;
+  }
+
+  return (
+
+    <main>
+
+  <Header />
+
+   <div className="flex-col flex justify-center items-center ">
+      <form   action="https://formspree.io/f/xbjnwwzj"  method="post" className="flex-col p-10 mt-8 mb-8 rounded-md w-[50rem] "
+      >
+        <div className="flex-col flex gap-2 mb-4">
+          <label className="text-xl" htmlFor="fullname">
+            Full Name
+          </label>
+          <input
+            required
+            name="fullname"
+            className="p-2 hover:border-blue-500 shadow-sm drop-shadow-lg shadow-blue-500 border-[1px] rounded-md outline-none"
+            type="text"
+            placeholder="Enter Your Name"
+            id="fullname"
+          />
+        </div>
+
+        <div className="flex-col flex gap-2 mb-4 ">
+          <label htmlFor="email" className="text-xl">
+            Email
+          </label>
+          <input
+            required
+            name="email"
+            className="p-2 hover:border-blue-500  shadow-sm drop-shadow-lg shadow-blue-500  border-[1px] rounded-md outline-none"
+            type="email"
+            placeholder="Enter Your Email"
+            id="email"
+          />
+
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+
+        <div className="flex-col flex gap-2 mb-4">
+          <label className="text-xl" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            required
+            name="message"
+            id="message"
+            placeholder="Type your message here..."
+            className="p-6 hover:border-blue-500   shadow-sm drop-shadow-lg shadow-blue-500  border-[1px] rounded-md outline-none"
+          ></textarea>
+
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+
+          <div className="">
+            <button
+            type="submit"
+            name="submit"
+              disabled={state.submitting}
+              className="py-2 px-4 bg-green-600 mt-2 rounded-md text-white"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+</main>
+ 
+  );
+}
