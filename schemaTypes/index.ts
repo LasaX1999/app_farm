98d6f28024226a65de933896334c6480203a2ffc
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-06-08 23:19:01 +0530

    delete other schemaTypes except user

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,4 @@
-import {eventType} from './eventType'
-import {artistType} from './artistType'
-import { venueType } from './venueType'
+
 import { userType } from './user'
 
-export const schemaTypes = [artistType, eventType, venueType,userType] 
+export const schemaTypes = [userType] 
