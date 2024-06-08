6bd67dbd340b362afc6a32a74a8998da4a956ba4
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-06-08 23:08:21 +0530

    resize  md font size 6xl to 5xl

--- app/components/Image-Silder.tsx
+++ app/components/Image-Silder.tsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 import React from "react";
 import { ImagesSlider } from "./ui/images-slider";
+import Link from "next/link";
 
 export default function ImagesSliderDemo() {
   const images = [
@@ -26,11 +27,14 @@ export default function ImagesSliderDemo() {
         }}
         className="z-50 flex flex-col justify-center items-center"
       >
-        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+        <motion.p className="font-bold text-xl md:text-5xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
         An online produce marketplace to buy and sell from the comfort of your own home.
         </motion.p>
         <button className="px-8 py-3 backdrop-blur-sm border bg-green-600/20 border-emerald-500/20 hover:border-emerald-600 text-white mx-auto text-center rounded-full relative mt-4">
+          <Link href={"/shop"}>
           <span className="text-xl">Shop Now</span>
+
+          </Link>
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
         </button>
       </motion.div>
