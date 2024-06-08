d809d98a11a77bac247836c01b81589274427fd5
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-06-08 21:40:00 +0530

    add sanity client

---
+++ sanity/client.ts
@@ -0,0 +1,36 @@
+'use client'
+import  imageUrlBuilder  from '@sanity/image-url';
+// import "server-only";
+
+import { createClient, type QueryParams } from "next-sanity";
+
+export const client = createClient({
+  projectId: "yp7c97qd",
+  dataset: "production",
+  apiVersion: "2024-01-01",
+  useCdn: false,
+});
+
+export async function sanityFetch<QueryResponse>({
+  query,
+  params = {},
+  tags,
+}: {
+  query: string;
+  params?: QueryParams;
+  tags?: string[];
+}) {
+  return client.fetch<QueryResponse>(query, params, {
+    next: {
+      revalidate: process.env.NODE_ENV === 'development' ? 30 : 3600,
+      tags,
+    },
+  });
+}
+
+
+const builder=imageUrlBuilder(client);
+
+export function urlFor(source:any){
+  return builder.image(source);
+}
