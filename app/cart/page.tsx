0147ebc01fcfd5440d4189b8927f62969ab9fc6f
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-04 20:49:39 +0530

    build cart page for shop cart item showing

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,15 +1,42 @@
-'use client'
-import React from 'react'
-import Header from '../components/Header'
-import Cart from '../components/Cart'
+// pages/cart.tsx
+'use client';
+import { useCart } from '@/app/context/CartContext';
+import Header from '@/app/components/Header';
+import Image from 'next/image';
 
-function page() {
+export default function Cart() {
+  const { cart } = useCart();
+
   return (
     <div>
-        <Header/>
-        <Cart/>
+      <Header />
+      <h1 className='text-3xl text-center pt-4'>Your Cart</h1>
+      <div className='flex flex-wrap items-center justify-center'>
+        {cart.length === 0 ? (
+          <p className='text-xl'>Your cart is empty.</p>
+        ) : (
+          cart.map((product) => (
+            <div key={product._id} className='m-5 border border-gray-300 p-4 rounded-lg max-w-xs'>
+              {product.imageUrl && (
+                <div className='relative w-full h-48'>
+                  <Image
+                    src={product.imageUrl}
+                    alt={product.title}
+                    layout='fill'
+                    objectFit='cover'
+                    className='rounded-lg'
+                  />
+                </div>
+              )}
+              <div className='flex flex-col text-center justify-center items-center border-t-2 border-green-400 mt-2 pt-2'>
+                <h2 className='text-xl font-semibold'>{product.title}</h2>
+                <p className='text-lg font-medium'>{product.price}</p>
+                <p className='text-sm text-gray-600'>{product.description}</p>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
-  )
+  );
 }
-
-export default page
