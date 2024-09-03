f4e72e31d1e7425e29d748d596ae5af71d870aae
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-09-03 09:27:59 +0530

    fix navlink userButton balance center position

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React from "react";
 import { HiShoppingCart } from "react-icons/hi2";
 import HoverMenu from "./HoverUser";
+import HoverSell from "./HoverSell";
 
 
 function Header() {
@@ -21,17 +22,21 @@ function Header() {
 
         <div className="flex items-center relative ">
           <Link href={'/shop'} className="lg:text-xl text-[18px] mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Shop </Link>
-          <Link href={'/admin'} className="lg:text-xl text-[18px]  mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Sell </Link>
+          {/* <Link href={'/admin'} className="lg:text-xl text-[18px]  mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Sell Product </Link> */}
+          {/* <Link href={'/request_admin'} className="lg:text-xl text-[18px]  mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Sell </Link> */}
+          <div className="flex items-center justify-center mt-6 ">
+           <HoverSell/>
+          </div>
           <Link href={'/about'} className="lg:text-xl text-[18px]  mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">About  </Link>
           <Link href={'/contact'} className="lg:text-xl text-[18px]  mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Contact </Link>
          <Link href={'/cart'}> <HiShoppingCart className="text-3xl cursor-pointer hover:scale-110" /></Link>
           {/* <div className="ml-2 bg-green-500 rounded-full w-5 h-5 flex items-center justify-center text-white text-sm font-semibold">1</div> */}
-          <div className="ml-3">
-          <UserButton afterSignOutUrl="/" />
+          <div className="ml-2 mt-[6px]">
+          <UserButton  afterSignOutUrl="/" />
           
           </div>
 
-          <div className="flex items-center justify-center mt-6 ">
+          <div className="flex items-center justify-center mt-5 scale-105 ">
           <HoverMenu />
           </div>
      
