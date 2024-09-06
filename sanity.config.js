0581a86730aa9b17f2abfae0e2ba5295cce57ab8
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-09-07 01:17:02 +0530

    fix getting data sanityIo

--- sanity.config.js
+++ sanity.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
-import {schemaTypes} from './schemaTypes'
+import { schemaTypes } from './schemaTypes';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const config = defineConfig({
-    projectId:'yp7c97qd',
-    dataset:'production',
-    basePath:"/admin",
-    title:'farm',
-    plugins:[deskTool()],
-    schema:{
-        types:schemaTypes},
+    projectId: 'yp7c97qd',
+    dataset: 'production',
+    basePath: "/admin",
+    title: 'farm',
+    plugins: [deskTool()],
+    schema: {
+        types: schemaTypes
+    },
+    api: {
+        token: process.env.SANITY_API_TOKEN,
+    },
+});
 
-})
-
-export default config
+export default config;
