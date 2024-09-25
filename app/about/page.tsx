2f54a749b3ef8359105dfca96624119ce743b4a2
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-09-25 15:04:13 +0530

    comment unwanted codes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,12 @@ import Footer from "../components/Footer";
 import NewFooter from "../components/NewFooter";
 import CilentSection from "../components/CilentSection";
 import FAQ from "../components/F&Q";
+import Image from "next/image";
+import { InfiniteSliderHoverSpeed } from "../components/InfiniteSliderHoverSpeed";
+import { InfiniteSlider } from "../components/framerMotion/Infinite";
 
 function page() {
+
   return (
     <main>
       <Header />
@@ -21,10 +25,118 @@ function page() {
   <h1 className="mt-10 max-w-lg bg-gradient-to-br from-teal-400 via-indigo-500 to-purple-600 bg-clip-text text-center text-4xl font-extrabold text-transparent sm:max-w-3xl sm:text-6xl">
     Farmers Market Direct is your source for all-natural food.
   </h1>
-  <span className="mt-8 max-w-2xl text-balance font-dm text-center text-lg leading-relaxed text-gray-700 sm:text-xl sm:leading-relaxed">
-    Farm Market Direct, opened in 2024 by Mr. Manoj Lasantha, came to fruition because we love the homegrown, homemade products found at the Farmers Market. Our platform connects these farmers and artisans to you, the consumer, 24/7! So, if you can’t make it to the Farmers Market, don't worry—you can still get high-quality, locally sourced products anytime.
+  <span className="mt-8 max-w-2xl text-balance font-roboto text-center text-lg leading-relaxed text-gray-700 sm:text-xl sm:leading-relaxed">
+    Farm Market Direct, opened in 2024 by Mr. Manoj Lasantha, came to fruition because we love the homegrown, homemade products found at the Farmers Market. Our platform connects these farmers and artisans to you, the consumer, 24/7 So, if you can’t make it to the Farmers Market, don't worry—you can still get high-quality, locally sourced products anytime.
   </span>
 </div>
+
+{/* <InfiniteSlider durationOnHover={75} gap={24}>
+      <Image
+        src='https://images.unsplash.com/photo-1702157896487-6dd66ce3c2c1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZhcm0lMjBwcm9kdWN0fGVufDB8MHwwfHx8Mg%3D%3D'
+        alt='Dean blunt - Black Metal 2'
+        fill
+        className='aspect-square w-[120px] rounded-[4px]'
+      />
+      <Image
+        src='https://images.unsplash.com/photo-1649779117064-107e63b88758?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFybSUyMHByb2R1Y3R8ZW58MHwwfDB8fHwy'
+        alt='Jungle Jack - JUNGLE DES ILLUSIONS VOL 2'
+        fill
+        className='aspect-square w-[120px] rounded-[4px]'
+      />
+      <Image
+        src='https://images.unsplash.com/photo-1461354464878-ad92f492a5a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTc1fHxmYXJtJTIwcHJvZHVjdHxlbnwwfDB8MHx8fDI%3D'
+        alt='Yung Lean - Stardust'
+        fill
+        className='aspect-square w-[120px] rounded-[4px]'
+      />
+      <Image
+        src='https://images.unsplash.com/photo-1448907503123-67254d59ca4f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fGZhcm0lMjBtZWF0fGVufDB8MHwwfHx8Mg%3D%3D'
+        alt='Lana Del Rey - Ultraviolence'
+        fill
+        className='aspect-square w-[120px] rounded-[4px]'
+      />
+      <Image
+        src='https://images.unsplash.com/photo-1456295198429-05d2315cfebb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGZhcm0lMjBmcnVpdHxlbnwwfDB8MHx8fDI%3D'
+        alt='A$AP Rocky - Tailor Swif'
+        fill
+        className='aspect-square w-[120px] rounded-[4px]'
+      />
+      <Image
+        src='https://images.unsplash.com/photo-1525904097878-94fb15835963?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fGZhcm0lMjBwcm9kdWN0fGVufDB8MHwwfHx8Mg%3D%3D'
+        alt='Midnight Miami (feat Konvy) - Nino Paid, Konvy'
+        fill
+        className='aspect-square w-[120px] rounded-[4px]'
+      />
+    </InfiniteSlider> */}
+ {/* Food Image Marquees */}
+ <div className=" flex items-center justify-center">
+      <div className="w-full overflow-hidden">
+        <div className="flex  space-x-8">
+          {/* Repeat this block for each image */}
+          <div className="relative h-60 w-72  ">
+            <Image
+              src="https://images.unsplash.com/photo-1702157896487-6dd66ce3c2c1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZhcm0lMjBwcm9kdWN0fGVufDB8MHwwfHx8Mg%3D%3D"
+              alt="Image 1"
+              fill
+              className="object-cover rounded-md "
+            />
+          </div>
+          {/* <div className="relative h-60 w-72">
+            <Image
+              src="https://images.unsplash.com/photo-1525904097878-94fb15835963?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fGZhcm0lMjBwcm9kdWN0fGVufDB8MHwwfHx8Mg%3D%3D"
+              alt="Image 2"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div> */}
+          {/* <div className="relative h-60 w-72 ">
+            <Image
+              src="https://images.unsplash.com/photo-1500651230702-0e2d8a49d4ad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTV8fGZhcm0lMjBwcm9kdWN0fGVufDB8MHwwfHx8Mg%3D%3D"
+              alt="Image 3"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div> */}
+
+          <div className="relative h-60 w-72 ">
+            <Image
+              src="https://images.unsplash.com/photo-1649779117064-107e63b88758?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFybSUyMHByb2R1Y3R8ZW58MHwwfDB8fHwy"
+              alt="Image 4"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+
+          <div className="relative h-60 w-72 ">
+            <Image
+              src="https://images.unsplash.com/photo-1461354464878-ad92f492a5a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTc1fHxmYXJtJTIwcHJvZHVjdHxlbnwwfDB8MHx8fDI%3D"
+              alt="Image 5"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+
+          <div className="relative h-60 w-72 ">
+            <Image
+              src="https://images.unsplash.com/photo-1448907503123-67254d59ca4f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fGZhcm0lMjBtZWF0fGVufDB8MHwwfHx8Mg%3D%3D"
+              alt="Image 6"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+
+          <div className="relative h-60 w-72 ">
+            <Image
+              src="https://images.unsplash.com/photo-1456295198429-05d2315cfebb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGZhcm0lMjBmcnVpdHxlbnwwfDB8MHx8fDI%3D"
+              alt="Image 7"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+          {/* Add more images as needed */}
+        </div>
+      </div>
+    </div>
 
       
         {/* <div className="mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-0 sm:gap-x-4">
@@ -58,7 +170,10 @@ function page() {
         </div> */}
      
       {/* <CilentSection/> */}
+      {/* <InfiniteSliderHoverSpeed/> */}
       <FAQ/>
+      
+     
       <NewFooter/>
     </main>
   );
