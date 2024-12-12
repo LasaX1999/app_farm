d2a1ffb4050ee875669f138cc9a4ec418c921bde
ItsLasa <manojlasantha306@gmail.com>
2024-12-12 18:28:35 +0530

    add sell guide for hover-sell navbar

--- app/components/HoverSell.tsx
+++ app/components/HoverSell.tsx
@@ -14,12 +14,13 @@ const HoverMenu = () => {
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className='mb-[23px]'>
-      <div className="lg:text-xl text-[18px] cursor-pointer mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Sell</div>
+      <div className="lg:text-xl  text-[18px] cursor-pointer mr-4 relative  w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Sell</div>
       </div>
       {isHovered && (
-        <div className="absolute top-full left-1/2 transform -translate-x-1/2 bg-white border border-gray-200 shadow-lg p-2 flex flex-col">
+        <div className="absolute rounded-lg top-full left-1/2 transform -translate-x-1/2 bg-white border border-gray-200 shadow-lg p-2 flex flex-col">
           <Link href="/admin" className="m-1 hover:underline">Sell</Link>
           <Link href="/request_admin" className="m-1 hover:underline">Request </Link>
+           <Link href="#" className="m-1 hover:underline">Sell Guide</Link>
         </div>
       )}
     </div>
