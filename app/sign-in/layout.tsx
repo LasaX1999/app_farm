bc1e0c20fbef32cdb93166d21d22ec1d56569c11
ItsLasa <manojlasantha306@gmail.com>
2024-12-13 01:27:44 +0530

    fixed error depolyement

--- app/sign-in/layout.tsx
+++ app/sign-in/layout.tsx
@@ -2,10 +2,10 @@
 import React from 'react'
 
 
-function layout({children}) {
+function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex items-center justify-center h-screen max-w-full'>
-        {children}
+      {children}
     </div>
   )
 }
